4b4bf5a747ae05c5af90fe4ea536e2fd9efcf58e
YashBagri <yashbagri2000@gmail.com>
2021-02-22 17:57:14 +0530

    changed few

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,25 +1,36 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoggedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
         title: Text("Welcome"),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Text("Logged In !!!"),
-          ),
-          FlatButton(
-            child: Text("Logout"),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Logged In !!!",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            FlatButton(
+              child: Text(
+                "Logout",
+                style: GoogleFonts.openSans(
+                  color: Theme.of(context).accentColor,
+                ),
+              ),
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+            )
+          ],
+        ),
       ),
     );
   }
